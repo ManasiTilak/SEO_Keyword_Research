a60f3fbd80e1d76ab4eab60ba2cb96cec45dc50f
Manasi <manasitilak12@gmail.com>
2023-06-17 04:35:59 +0530

    cleaned and formatted scrapper.py

--- scraper.py
+++ scraper.py
@@ -6,32 +6,33 @@ from bs4 import BeautifulSoup
 
 # Add header User-Agent which is a string to tell the server what kind of device you are accessing the page with.
 #This is very important.
+
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
 
-# Make two strings with default google search URL
-# 'https://google.com/search?q=' and
-# our customized search keyword.
-# Concatenate them
-
-text= "geekforgeeks"
+'''
+Make two strings: 
+1) default google search URL
+2) customized search keyword.
+Concatenate them
+'''
+text= "how to format beautiful soup"
 url = 'https://google.com/search?q=' + text
 
-
-# Fetch the URL data using requests.get(url),
-# store it in a variable, r.
+# Fetch the URL data using requests. Get data content.
 r = requests.get( url, headers=headers )
 htmlContent = r.content
-# print(htmlContent)
 
 # Creating soup from the fetched request
 soup = BeautifulSoup(htmlContent, "html.parser")
-# print(soup.prettify())
 
-# # HTML Tree traversal
-# div = soup.find_all("div", class_="main")
-# print(div)
+# HTML Tree traversal
+
+# this finds the string in thr div "result-stats". the result should be something like "About 1234 results"
 total_results_text = soup.find("div", {"id": "result-stats"}).find(string=True, recursive=False)
-results_num = ''.join([num for num in total_results_text if num.isdigit()]) # now will clean it up and remove all the characters that are not a number .
+
+# clean it up the result and remove all the characters that are not a number .
+results_str = ''.join([num for num in total_results_text if num.isdigit()]) 
+results_num = int(results_str)
 print(results_num)
 
 
