53fa5fc3366ea9503cdd529213d5ce7ae4403c4e
Manasi <manasitilak12@gmail.com>
2023-06-16 23:18:14 +0530

    initial comment

---
+++ scraper.py
@@ -0,0 +1,23 @@
+# Import the beautifulsoup
+# and request libraries of python.
+import requests
+import bs4
+
+# Make two strings with default google search URL
+# 'https://google.com/search?q=' and
+# our customized search keyword.
+# Concatenate them
+text= "trial"
+url = 'https://google.com/search?q=' + text
+
+# Fetch the URL data using requests.get(url),
+# store it in a variable, request_result.
+request_result=requests.get( url )
+
+# Creating soup from the fetched request
+soup = bs4.BeautifulSoup(request_result.text,
+						"html.parser")
+# print(soup)
+number = soup.find('div',{'id':'resultStats'})
+print(number)
+
